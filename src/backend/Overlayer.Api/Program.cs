0d49aa81e20db23744d3a92288ea148b9dd7b55f
jonathan-jesus <sousjonathan@gmail.com>
2026-04-28 13:18:31 -0400

    feat(api): ensure job id is a valid guid for request-upload-urls

--- src/backend/Overlayer.Api/Program.cs
+++ src/backend/Overlayer.Api/Program.cs
@@ -6,6 +6,8 @@ var app = builder.Build();
 
 app.MapGet("/api/jobs/{jobId}/upload-urls", (string jobId, [FromHeader(Name = "X-Session-ID")] Guid sessionId) =>
 {
+    if (!Guid.TryParse(jobId, out _)) return Results.BadRequest();
+
     return Results.Ok(new
     {
         jobId,

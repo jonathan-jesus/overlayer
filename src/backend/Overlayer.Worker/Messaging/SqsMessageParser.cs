622da92090103fed1977fe06731e779066251bf1
jonathan-jesus <sousjonathan@gmail.com>
2026-05-18 23:13:50 -0400

    feat(worker): implement SqsPollingLoop message dispatch

---
+++ src/backend/Overlayer.Worker/Messaging/SqsMessageParser.cs
@@ -0,0 +1,6 @@
+namespace Overlayer.Worker.Messaging;
+
+public static class SqsMessageParser
+{
+    public static (string SessionId, string JobId)? Parse(string messageBody) => throw new NotImplementedException();
+}
--- src/backend/Overlayer.Worker/Messaging/SqsPollingLoop.cs
+++ src/backend/Overlayer.Worker/Messaging/SqsPollingLoop.cs
@@ -1,4 +1,5 @@
 using Amazon.SQS;
+using Amazon.SQS.Model;
 using Overlayer.Worker.Configuration;
 using Overlayer.Worker.Processing;
 
@@ -6,8 +7,34 @@ namespace Overlayer.Worker.Messaging;
 
 public class SqsPollingLoop
 {
-    public SqsPollingLoop(IAmazonSQS sqs, SqsOptions options, IJobProcessor processor) { }
+    private readonly IAmazonSQS _sqs;
+    private readonly SqsOptions _options;
+    private readonly IJobProcessor _processor;
+    public SqsPollingLoop(IAmazonSQS sqs, SqsOptions options, IJobProcessor processor)
+    {
+        _processor = processor;
+        _options = options;
+        _sqs = sqs;
+    }
 
     public async Task RunOnceAsync(CancellationToken ct = default)
-        => throw new NotImplementedException();
+    {
+        var sqsOptions = _options;
+        var response = await _sqs.ReceiveMessageAsync(new ReceiveMessageRequest
+        {
+            QueueUrl = sqsOptions.QueueUrl,
+            MaxNumberOfMessages = 10,
+            WaitTimeSeconds = sqsOptions.WaitTimeSeconds
+        }, ct);
+
+        foreach (var message in response.Messages)
+        {
+            var jobInfo = SqsMessageParser.Parse(message.Body);
+            if (jobInfo != null)
+            {
+                await _processor.HandleAsync(jobInfo.Value.SessionId, jobInfo.Value.JobId);
+            }
+            await _sqs.DeleteMessageAsync(sqsOptions.QueueUrl, message.ReceiptHandle, ct);
+        }
+    }
 }
